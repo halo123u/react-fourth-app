e8abe0568c9b6152c72a9a03e695994f32e61506
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-04-14 22:59:20 -0400

    add Delete route to remove posts

--- src/components/post_show.js
+++ src/components/post_show.js
@@ -1,11 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import {fetchPost} from '../actions/index';
+import {fetchPost, deletePost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostShow extends Component{
+     static contextTypes = {
+        router: PropTypes.object
+    };
+
     componentWillMount() {
         this.props.fetchPost(this.props.params.id);
     }
+
+    onDeleteClick(){
+        this.props.deletePost(this.props.params.id)
+        .then(()=>{
+            this.context.router.push('/');
+        });
+    }
+    
     render(){
         const {post} =this.props;
         if(!post) {
@@ -13,6 +26,12 @@ class PostShow extends Component{
         }
         return (
             <div>
+                <Link to="/"  className="">Back to Index</Link>
+                <button 
+                    onClick={this.onDeleteClick.bind(this)}
+                    className="btn btn-danger pull-xs-right">
+                    Delete Post
+                </button>
                 <h3>{post.title}</h3>
                 <h6>Categories: {post.categories}</h6>
                 <p>{post.content}</p>
@@ -24,4 +43,4 @@ function mapStateToProps(state) {
     return { post: state.posts.post};
 }
 
-export default connect(mapStateToProps, {fetchPost} )(PostShow);
+export default connect(mapStateToProps, {fetchPost, deletePost} )(PostShow);
